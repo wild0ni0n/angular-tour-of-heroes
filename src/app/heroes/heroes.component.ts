4c6587f73daae167d208168cd5401d96c8b7ef55
wild0ni0n <yuisuwako@gmail.com>
2023-01-03 18:06:46 +0900

    update heroes component logic

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstrom'
@@ -15,4 +19,9 @@ export class HeroesComponent {
   constructor() { }
 
   ngOnInit(): void { }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
